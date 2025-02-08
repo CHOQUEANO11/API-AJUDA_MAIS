ae84e5a8f5d7c68b3295df7695bb1c4d178bd934
Nilson Silva <nilsonchoque11@gmail.com>
2025-02-08 17:06:18 -0300

    Connection in Mongo Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB conectado com sucesso!');
   } catch (error) {
     console.error('Erro ao conectar no MongoDB:', error);
@@ -14,3 +11,8 @@ const connectDB = async () => {
 };
 
 export default connectDB; 
+
+// {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }
