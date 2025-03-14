e341062dc28be86bbc7fca86adb307bae434e434
Nilson Silva <nilsonchoque11@gmail.com>
2025-03-13 22:59:45 -0300

    create schema chat

--- src/routes/sessionChatRoutes.js
+++ src/routes/sessionChatRoutes.js
@@ -1,7 +1,7 @@
 // src/routes/orgRoutes.js
 
 import express from 'express';
-import {listAllSessions, getSessionById, createSession, updateSession, finalizeSession} from '../../src/controllers/sessionChatEvaluationController.js';
+import {listAllSessions, getSessionById, createSession, updateSession, finalizeSession} from '../controllers/sessionChatController.js';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 
